661e82651c6d988692923e263235123a94ebd61b
higoreugenio <115594794+higoreugenio@users.noreply.github.com>
2023-08-14 16:51:01 -0300

    Add files via upload

---
+++ atividadeJava/src/aplication/ConversorDeMoeda.java
@@ -0,0 +1,36 @@
+package aplication;
+
+//Desenvolvedor: Higor Eugenio
+//Github: https://github.com/higoreugenio/Casa-de-cambio
+
+
+public class ConversorDeMoeda {
+	
+	
+	private  double dolar = 4.91;
+	private double euro = 5.38;
+	private  double chineseYuan = 0.68;
+	public double valor = 0.00;
+	public double valorPago = 0.00;
+	public int opcao;
+	
+	public double calcularValor() {
+	
+	if (opcao == 1) {
+		valorPago = valor * dolar;
+		
+	}else if (opcao == 2) {
+		valorPago = valor * euro;
+		
+	}else if (opcao == 3) {
+		valorPago = valor * chineseYuan;
+			
+		}
+	
+		return valorPago;
+	}
+	
+}
+
+
+	
---
+++ atividadeJava/src/entities/CasaDeCambio.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.util.Scanner;
+
+import aplication.ConversorDeMoeda;
+
+//Desenvolvedor: Higor Eugenio
+//Github: https://github.com/higoreugenio/Casa-de-cambio
+
+public class CasaDeCambio {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		ConversorDeMoeda conversor = new ConversorDeMoeda();
+		
+		System.out.println("Escolha a moeda que deseja adquirir");
+		System.out.println("1 - Dólar");
+		System.out.println("2 - Euro");
+		System.out.println("3 - Chinese Yuan");
+		conversor.opcao = sc.nextInt();
+		
+		if (conversor.opcao < 1 || conversor.opcao > 3) {
+			System.out.println("A opção que você digitou não é válida!");
+		}else{
+		System.out.print("Informe o valor que deseja comprar: ");
+		conversor.valor = sc.nextDouble();
+	
+		System.out.printf("O valor a ser pago deve ser: R$%.2f", conversor.calcularValor());
+		
+		}
+	
+		sc.close();
+		
+	}
+
+}
